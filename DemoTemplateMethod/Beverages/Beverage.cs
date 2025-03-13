cd62a37ce01b329217169223edf8bc88abec1261
nhh1844 <Administrator@DESKTOP-P8G7D3C>
2025-03-13 20:40:22 +0700

    update class design

--- DemoTemplateMethod/Beverages/Beverage.cs
+++ DemoTemplateMethod/Beverages/Beverage.cs
@@ -1,43 +1,63 @@
-﻿namespace DemoTemplateMethod.Beverages
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DemoTemplateMethod.Beverages
 {
     // Base class Beverage
     public abstract class Beverage
     {
+        public int currentStep = 0;
+        public string CurrentStepDescription = "";
+        public string CurrentStepImage = "";
         public string Name { get; protected set; }
         public string Description { get; protected set; }
         public string MenuImagePath { get; protected set; }
 
+        public List<string> StepDescriptions { get; private set; } = new List<string>();
+        public List<string> StepImages { get; private set; } = new List<string>();
+
+
         // Template method
         public void Prepare()
         {
+
             PrepareIngredients();
+            AddStepDescriptionAndImage();
+
             BrewMainIngredient();
+            AddStepDescriptionAndImage();
+
             AddCustomIngredients();
+            AddStepDescriptionAndImage();
+
             FinishAndDecorate();
+            AddStepDescriptionAndImage();
+
             Serve();
+            AddStepDescriptionAndImage();
+
         }
 
-        protected virtual void PrepareIngredients()
+        private void AddStepDescriptionAndImage()
         {
-            // Common step for all beverages
+            StepDescriptions.Add(CurrentStepDescription);
+            StepImages.Add(CurrentStepImage);
         }
+
+        protected abstract void PrepareIngredients();
 
         protected abstract void BrewMainIngredient();
         protected abstract void AddCustomIngredients();
 
-        protected virtual void FinishAndDecorate()
-        {
-            // Common step for all beverages
-        }
+        protected abstract void FinishAndDecorate();
 
-        protected virtual void Serve()
-        {
-            // Common step for all beverages
-        }
+        protected abstract void Serve();
 
-        // Method to get information about the steps
-        public abstract string[] GetStepDescriptions();
-        public abstract string[] GetStepImagePaths();
+
+
+
+
     }
 }
-
